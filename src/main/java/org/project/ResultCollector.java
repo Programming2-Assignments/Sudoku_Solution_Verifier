fbe594af5579c0d65079eb2159e0d63aafa9848d
Youssef Amr Mohamed Abd Wareth <es.yousef.amr2023@alexu.edu.eg>
2025-11-30 19:27:23 +0200

    finishing the result collector for that handels multithreading

--- src/main/java/org/project/ResultCollector.java
+++ src/main/java/org/project/ResultCollector.java
@@ -1,7 +1,32 @@
 package org.project;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ResultCollector {
+    private final List<DuplicateRecord> duplicateRows = Collections.synchronizedList(new ArrayList<>());
+    private final List<DuplicateRecord> duplicateCols = Collections.synchronizedList(new ArrayList<>());
+    private final List<DuplicateRecord> duplicateBoxes = Collections.synchronizedList(new ArrayList<>());
 
+    public void addDuplicateRows(List<DuplicateRecord> duplicateRows) {
+        this.duplicateRows.addAll(duplicateRows);
+    }
+    public void addDuplicateCols(List<DuplicateRecord> duplicateCols) {
+        this.duplicateCols.addAll(duplicateCols);
+    }
+    public void addDuplicateBoxes(List<DuplicateRecord> duplicateBoxes) {
+        this.duplicateBoxes.addAll(duplicateBoxes);
+    }
 
+    public List<DuplicateRecord> getDuplicateRows() {
+        return duplicateRows;
+    }
+    public List<DuplicateRecord> getDuplicateCols() {
+        return duplicateCols;
+    }
+    public List<DuplicateRecord> getDuplicateBoxes() {
+        return duplicateBoxes;
+    }
 
 }
